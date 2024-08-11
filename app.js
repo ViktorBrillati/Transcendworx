10a2e5c0e381c346de3c3e4067bee7575afe1e36
Viktor Brillati <Vbrillati@gmail.com>
2024-08-11 15:55:33 -0400

    added new hero poto and updated styles

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.post('/users/login', redirectIfAuthenticatedMiddleware, loginUserController)
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-    port = 8000;
+    port = 3000;
 }
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
